0940ab69eb5e2d0a4e3508d29f2c6837af9f64c8
Shaun Limbeek <slimbeek@Shauns-MBP.attlocal.net>
2022-06-21 13:50:11 -0700

    add user controls via installing devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_183620) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_204900) do
   create_table "candidates", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -40,6 +40,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_183620) do
     t.string "equity_band"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
